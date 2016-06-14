a8a1093ead3fa5759af9235f197737bd9607e7fa
leofreitas <leonardofreitas159@gmail.com>
2016-06-14 15:59:24 -0300

    teste Bd criar tabela 5

--- src/main/java/com/app/apiexpotaxi/Index.java
+++ src/main/java/com/app/apiexpotaxi/Index.java
@@ -14,35 +14,5 @@ package com.app.apiexpotaxi;
  */
 
 public class Index{
-    
-    private Integer id;
-    
-    private String nome;
-
-    public Index(Integer id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Index() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    
-    
-    
+        
 }
